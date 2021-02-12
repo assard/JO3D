097705a944d91159614732cc6e9245a7ee034b1c
formation <formation@ubuntu.ubuntu-domain>
2021-02-12 11:34:04 +0100

    Load of stade de france is ok

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,10 +1,10 @@
-import * as THREE from 'https://unpkg.com/three/build/three.module.js';
-import { GLTFLoader } from 'https://unpkg.com/three/examples/jsm/loaders/GLTFLoader.js';
+// import * as THREE from 'https://unpkg.com/three/build/three.module.js';
+// import { GLTFLoader } from 'https://unpkg.com/three/examples/jsm/loaders/GLTFLoader.js';
 
 const viewerDiv = document.getElementById('viewerDiv');
 const position = {
-    coord: new itowns.Coordinates('EPSG:4326', 2.3488, 48.8534),
-    range: 25000
+    coord: new itowns.Coordinates('EPSG:4326', 2.36,48.924444),
+    range: 2000
 };
 const view = new itowns.GlobeView(viewerDiv, position);
 
@@ -39,24 +39,38 @@ const elevationLayer = new itowns.ElevationLayer('MNT_WORLD', {
 view.addLayer(elevationLayer);
 
 async function loadGLTF(){
-    let gltfLoader = new GLTFLoader();
-    gltfLoader.load('../data/aviva_stadium/scene.gltf', gltf => {
-        console.log(gltf);
 
-        //Base on the example https://github.com/iTowns/itowns/blob/master/examples/misc_collada.html
 
-        //Get the ThreeGroup
-        let model = gltf.scene;
+    let gltf = await ThreeLoader.load('GLTF','../data/aviva_stadium/scene.gltf');
 
-        //Building coordinate
-        let coord = new itowns.Coordinates('EPSG:4326',48.924444,2.36,46);
+    //Base on the example https://github.com/iTowns/itowns/blob/master/examples/misc_collada.html
 
-        //Set the model position to the coord define before
-        model.position.copy(coord.as(view.referenceCrs));
+    //Get the ThreeGroup
+    let model = gltf.scene;
 
-        //Align up vector with geodesic normal
-        model.lookAt(model.position.clone().add(coord.geodesicNormal));
-    });
+    //Building coordinate
+    let coord = new itowns.Coordinates('EPSG:4326',2.36,48.924444,46);
+
+    //Set the model position to the coord define before
+    model.position.copy(coord.as(view.referenceCrs));
+
+    //Align up vector with geodesic normal
+    model.lookAt(model.position.clone().add(coord.geodesicNormal));
+
+    // user rotate building to align with ortho image
+    model.rotateZ(-Math.PI/12);
+    model.rotateX(Math.PI/2);
+    model.rotateY(Math.PI/8);
+    model.scale.set(0.002, 0.002, 0.002);
+
+
+    // update coordinate of the mesh
+    model.updateMatrixWorld();
+
+    view.scene.add(model);
+
+    //
+    //itowns.CameraUtils.sequenceAnimationsToLookAtTarget(view, camera, pathTravel);
 }
 
 loadGLTF();
